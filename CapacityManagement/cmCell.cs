d19a198e168906edee5da184da11d093fb430ae8
AbdelrahmanSE <abdelrahmanse@hotmail.com>
2017-02-18 14:25:34 +0200

    Creating Project and Ericsson Overshooting Check
    - Creating the project
    - Checking if the cells of Ericsson are overshooting

---
+++ CapacityManagement/cmCell.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapacityManagement
+{
+    public class cmCell
+    {
+        public string cellName { get; set; }
+        public string cellType { get; set; }
+        public struct recommendationsType
+        {
+            float antennaDownTilt;
+        }
+        public recommendationsType recommendations = new recommendationsType();
+
+        public cmCell()
+        {
+
+        }
+        public bool isCellOverShooting()
+        {
+
+            return true;
+        }
+    }
+}
---
+++ CapacityManagement/cmSystem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapacityManagement
+{
+    public static class cmSystem
+    {
+        public static void cmGetCellNames()
+        {
+
+        }
+        public static void cmGetOverShootingCellNames()
+        {
+
+        }
+        public static void cmUpdateCells(cmCell cellNames,cmCell overShooting)
+        {
+
+        }
+    }
+}
